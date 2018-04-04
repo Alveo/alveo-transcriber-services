b450945349b028671cafe99e72c50bd4daadaddd
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-04-04 17:46:05 +1000

    Clarify user_id purpose

--- application/users/model.py
+++ application/users/model.py
@@ -4,18 +4,18 @@ from application import app, db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(48), nullable=False, unique=True)
+    alveo_id = db.Column(db.String(48), nullable=False, unique=True)
     api_key = db.Column(db.String(64), nullable=False, unique=True)
 
-    def __init__(self, user_id):
-        self.user_id = user_id
+    def __init__(self, alveo_id):
+        self.alveo_id = alveo_id 
         self.generate_token()
 
     def __repr__(self):
-        return str(self.user_id)
+        return str(self.alveo_id)
 
     def __str__(self):
-        return str(self.user_id)
+        return str(self.alveo_id)
 
     def generate_token(self):
         self.api_key = token_hex(app.config['TOKEN_LENGTH'])
