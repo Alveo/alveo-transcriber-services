f75b6053eb2848114cbe3d393bd71f9e76045d89
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-04-09 13:15:51 +1000

    Update naming conventions, note JSON storage

--- application/segmentation/model.py
+++ application/segmentation/model.py
@@ -1,16 +1,21 @@
 from application import db
 
+# See: https://bitbucket.org/zzzeek/sqlalchemy/issues/3850/request-sqlite-json1-ext-support
+# Support for JSON storage in sqlite (used in unit testing) coming, will double over for postgresql too
+#from sqlalchemy import JSON
+
 class CachedSegmentationResult(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    identifier = db.Column(db.String(512), nullable=False, unique=True)
-    data = db.Column(db.String, nullable=False)
+    alveo_id = db.Column(db.String(512), nullable=False, unique=True)
+    #data = db.Column(JSON)
+    data = db.Column(db.String)
 
-    def __init__(self, identifier, data):
-        self.identifier = identifier
+    def __init__(self, alveo_id, data):
+        self.alveo_id = alveo_id
         self.data = data
 
     def __repr__(self):
-        return str(self.identifier)
+        return str(self.alveo_id)
 
     def __str__(self):
-        return str(self.identifier)
+        return str(self.alveo_id)
