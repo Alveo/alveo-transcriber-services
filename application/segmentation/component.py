edb40c56c66c088a2b99ced93b3700411a92814c
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-04-09 13:16:39 +1000

    Partially implement cache, refactor

--- application/segmentation/component.py
+++ application/segmentation/component.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+from urllib.parse import urlparse
 
 import pyalveo
 from flask import jsonify, abort, g, request
@@ -10,40 +11,74 @@ from application.segmentation.audio_segmentor import AudioSegmentor
 from application.segmentation.model import CachedSegmentationResult
 from application.users.auth import auth_required, get_api_access
 
+def shorten_id(document_id):
+    url = urlparse(document_id)
+    url = url.path.split('/catalog/')[1]
+    return url
+
+def cache_result(document_id, result):
+    document_id = shorten_id(document_id)
+    cached_result = CachedSegmentationResult(document_id, str(result))
+    db.session.add(cached_result)
+    db.session.commit()
+
+def get_cached_result(document_id):
+    document_id = shorten_id(document_id)
+    result = CachedSegmentationResult.query.filter(CachedSegmentationResult.alveo_id == document_id).first()
+    if result is None:
+        return None
+
+    return jsonify(result.data)
+
+def remote_segment(document_id):
+    audio_result = None
+    try:
+        audio_result = client.get_document(document_id)
+    except:
+        pass
+
+    if audio_result is None:
+        return None
+
+    tmp_path = '/tmp/alveo/'+str(uuid.uuid4());
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, 'wb') as f:
+        f.write(audio_result)
+
+    result = AudioSegmentor(file_path).segment()
+
+    cache_result(document_id, result)
+
+    try:
+        os.remove(tmp_path)
+    except:
+        pass
+
+    return result
+
+    url = urlparse(document_id)
+    url = url.path.split('/catalog/')[1]
 class SegmentorService(MethodView):
     @auth_required
     def get(self, identifier=None):
         document_id = request.args.get('document_id')
-        if not document_id:
+
+        if "/" not in urlparse(document_id).path:
             abort(400, 'Request did not receive an Alveo document identifier to segment.')
 
         aas_key = get_api_access(g.user.api_key)
         if aas_key is None:
             abort(400, 'You\'re not authorised to access the Alveo API, register your API key at /authorize first.')
 
-        client = pyalveo.Client(api_url='https://app.alveo.edu.au/', api_key=aas_key, use_cache=False, cache_dir=None)
+        client = pyalveo.Client(api_url='https://app.alveo.edu.au/', api_key=aas_key, use_cache=False, update_cache=False, cache_dir=None)
 
-        # TODO cache reading, check if user has permission to access file somehow
-        audio_result = None
-        try:
-            audio_result = client.get_document(document_id)
-        except:
-            pass
-
-        if audio_result is None:
-            abort(400, 'Could not access requested document.')
-
-
-        
-        # TODO - Cleanup
-        file_path = '/tmp/alveo/'+str(uuid.uuid4());
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        with open(file_path, 'wb') as f:
-            f.write(audio_result)
-
-        testseg = AudioSegmentor(file_path)
-        result = testseg.segment()
-        # TODO cache writing
+        # TODO check Alveo permissions before accessing cache
+        #   Note: request submitted to PyAlveo repository for permission checking
+        result = get_cached_result(document_id)
+        if result is None:
+            result = remote_segment(document_id) 
+            if result is None:
+                abort(400, 'Could not access requested document.')
 
         response = {'status': 200, 'result': result}
 
