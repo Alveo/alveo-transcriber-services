0291b2924888088d3228d905e41703ca09c25b49
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-04-09 13:27:17 +1000

    Bugfixes, finish implementing cache

--- application/segmentation/component.py
+++ application/segmentation/component.py
@@ -18,6 +18,8 @@ def shorten_id(document_id):
 
 def cache_result(document_id, result):
     document_id = shorten_id(document_id)
+    if len(document_id) is 0:
+        raise Exception("Attempting to store something to the cache without a valid document identifier")
     cached_result = CachedSegmentationResult(document_id, str(result))
     db.session.add(cached_result)
     db.session.commit()
@@ -30,7 +32,9 @@ def get_cached_result(document_id):
 
     return jsonify(result.data)
 
-def remote_segment(document_id):
+def remote_segment(document_id, aas_key):
+    client = pyalveo.Client(api_url='https://app.alveo.edu.au/', api_key=aas_key, use_cache=False, update_cache=False, cache_dir=None)
+
     audio_result = None
     try:
         audio_result = client.get_document(document_id)
@@ -40,7 +44,7 @@ def remote_segment(document_id):
     if audio_result is None:
         return None
 
-    tmp_path = '/tmp/alveo/'+str(uuid.uuid4());
+    file_path = '/tmp/alveo/'+str(uuid.uuid4());
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, 'wb') as f:
         f.write(audio_result)
@@ -56,27 +60,23 @@ def remote_segment(document_id):
 
     return result
 
-    url = urlparse(document_id)
-    url = url.path.split('/catalog/')[1]
 class SegmentorService(MethodView):
     @auth_required
     def get(self, identifier=None):
         document_id = request.args.get('document_id')
 
-        if "/" not in urlparse(document_id).path:
+        if "/" not in str(urlparse(document_id).path):
             abort(400, 'Request did not receive an Alveo document identifier to segment.')
 
         aas_key = get_api_access(g.user.api_key)
         if aas_key is None:
             abort(400, 'You\'re not authorised to access the Alveo API, register your API key at /authorize first.')
 
-        client = pyalveo.Client(api_url='https://app.alveo.edu.au/', api_key=aas_key, use_cache=False, update_cache=False, cache_dir=None)
-
         # TODO check Alveo permissions before accessing cache
         #   Note: request submitted to PyAlveo repository for permission checking
         result = get_cached_result(document_id)
         if result is None:
-            result = remote_segment(document_id) 
+            result = remote_segment(document_id, aas_key) 
             if result is None:
                 abort(400, 'Could not access requested document.')
 
