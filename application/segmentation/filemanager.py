dd283fd032a34b4f154bf643704ce624f0360a49
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-04-09 15:41:13 +1000

    Deprecate save, make more generic

--- application/segmentation/filemanager.py
+++ application/segmentation/filemanager.py
@@ -3,27 +3,27 @@ import os
 from application import app
 
 class FileManager:
-    def __init__(self, filename, data):
-        self.filename = filename
-        self.data = data
+    def __init__(self, file_path, file_data):
+        self.file_path = file_path
+        self._validate_path()
+
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as f:
+            f.write(file_data)
+
+        self.stored = True
         
     def _validate_path(self):
         """ Validates whether the directory exists or not. Creates the directory if it does not exist. Should not be called from outside the class. """
-        directory = os.path.dirname(os.path.realpath(self.filename))
+        directory = os.path.dirname(os.path.realpath(self.file_path))
         if not os.path.exists(directory):
             os.makedirs(directory)
-
-    def save(self):
-        """ Saves data to the file location. """
-        self._validate_path()
-        self.data.save(self.filename)
-        self.stored = True
 
     def cleanup(self):
         """ Erases any downloaded data. """
         if self.stored:
             try:
-                os.remove(self.filename)
+                os.remove(self.file_path)
             except:
                 pass
             self.stored = False
