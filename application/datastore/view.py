6562122ac01ea7902f7630adb95dd94a8ec3fa2d
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-02 11:59:40 +1000

    Implement module events

--- application/datastore/view.py
+++ application/datastore/view.py
@@ -2,12 +2,14 @@ from flask import abort, request, jsonify
 from flask.views import MethodView
 
 from application.auth.required import auth_required
+from application.misc.events import get_handler_for
 
 class DatastoreAPI(MethodView):
     @auth_required
     def get(self):
         storage_key = request.args.get('storage_key')
         revision = request.args.get('revision')
+        api_domain = request.headers.get('X-Api-Domain')
 
         if storage_key is None:
             abort(400, "storage_key not provided")
@@ -16,24 +18,29 @@ class DatastoreAPI(MethodView):
         if revision is None:
             revision = "latest" # TODO
 
-        # Find the thing, match it, invoke it
-        # match = ?
+        store = get_handler_for(api_domain, 'retrieve')
 
-        if match is None:
-            abort(404, "No matches found");
+        if store is None:
+            abort(404, "No store matches the specified domain");
+
+        results = store.handle(storage_key, revision)
+
+        if results is None:
+            abort(404, "No data found for specified storage_key")
 
         return jsonify(
                     {
-                        "results": match.value
+                        "revision": results.revision,
+                        "data": results.value
                     }
                 )
-
 
     @auth_required
     def post(self):
         data = request.get_json()
         storage_key = None
         storage_value = None
+        api_domain = request.headers.get('X-Api-Domain')
 
         try:
             storage_key = data['storage_key']
@@ -47,12 +54,17 @@ class DatastoreAPI(MethodView):
         if storage_value is None:
             abort(400, "storage_value not provided")
 
-        # Find the thing, match it, invoke it
+        store = get_handler_for(api_domain, 'store')
+
+        if store is None:
+            abort(404, "No store matches the specified domain");
+
+        result = store.handle(storage_key, storage_value)
 
         return jsonify(
                     {
                         "key": storage_key,
-                        "revision": "latest",
+                        "revision": result.revision,
                     }
                 )
 
