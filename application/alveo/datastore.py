7c8733db09c89fc177d72529ca3ad663d2465587
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-07 16:20:56 +1000

    Handle storage with expected values better

--- application/alveo/datastore.py
+++ application/alveo/datastore.py
@@ -1,3 +1,5 @@
+import json
+
 from pyalveo import *
 from flask import g
 
@@ -18,19 +20,20 @@ def alveo_retrieve(storage_key, revision):
     if ref is None:
         return None
 
-    return ref.value.decode()
+    return json.loads(ref.value.decode())
 
 @auth_required
 @handle_api_event("alveo", "store")
 def alveo_store(storage_key, storage_value):
     revision = "latest"
     model = Datastore.query.filter(Datastore.key == storage_key).filter(Datastore.revision == revision).filter(Datastore.user_id == g.user.id).first()
-
+    
+    data = json.dumps(storage_value).encode()
     if model is None:
-        model = Datastore(storage_key, storage_value.encode(), revision, g.user)
+        model = Datastore(storage_key, data, revision, g.user)
         db.session.add(model)
     else:
-        model.value = storage_value.encode()
+        model.value = data
 
     db.session.commit()
 
