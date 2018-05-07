f5a3d01a2e172147b971536458a5590201ad0043
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-07 19:31:33 +1000

    Remove unneeded auth decorator

--- application/alveo/datastore.py
+++ application/alveo/datastore.py
@@ -1,20 +1,23 @@
 import json
 
 from pyalveo import *
-from flask import g
+from flask import g, abort
 
 from application import app, db
 from application.users.model import User
 from application.misc.events import handle_api_event
-from application.auth.required import auth_required
 
 from application.datastore.model import Datastore
 
 from struct import pack, unpack
 
-@auth_required
 @handle_api_event("alveo", "retrieve")
 def alveo_retrieve(storage_key, revision):
+    if storage_key is None or len(storage_key) < 2:
+        abort(400, "Key is invalid or too short")
+
+    storage_key = "alveo:"+storage_key
+
     ref = Datastore.query.filter(Datastore.key == storage_key).filter(Datastore.revision == revision).filter(Datastore.user_id == g.user.id).first()
 
     if ref is None:
@@ -22,10 +25,15 @@ def alveo_retrieve(storage_key, revision):
 
     return json.loads(ref.get_data())
 
-@auth_required
 @handle_api_event("alveo", "store")
 def alveo_store(storage_key, storage_value):
     revision = "latest"
+    
+    if storage_key is None or len(storage_key) < 2:
+        abort(400, "Key is invalid or too short")
+
+    storage_key = "alveo:"+storage_key
+
     model = Datastore.query.filter(Datastore.key == storage_key).filter(Datastore.revision == revision).filter(Datastore.user_id == g.user.id).first()
     
     data = json.dumps(storage_value)
