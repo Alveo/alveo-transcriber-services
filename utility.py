25e9e42fe60709cff72181a98ee3e040eb939af1
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-07 19:59:51 +1000

    Add push_transcriptions (zip)

--- utility.py
+++ utility.py
@@ -1,6 +1,12 @@
+import io
+import urllib.request
+import zipfile
+import json
+
 import click
 
-from application import db
+from application import app, db
+from application.datastore.model import Datastore
 
 @click.group()
 def cli():
@@ -12,7 +18,29 @@ def init_db():
     db.create_all()
     click.echo("Database has been rebuilt.")
 
+@click.command()
+@click.argument('url')
+def push_transcriptions(url):
+    data = Datastore.query.all()
+
+    archive = io.BytesIO()
+    with zipfile.ZipFile(archive, mode='w') as zf:
+        for transcription in data:
+            data = {
+                    "id": transcription.key,
+                    "creator": transcription.user.remote_user_id,
+                    "revision": transcription.revision,
+                    "transcription": transcription.value.decode()
+                    }
+            zf.writestr('%s.json' % transcription.key, json.dumps(data))
+    archive.seek(0)
+
+    request = urllib.request.Request(url, data=archive)
+    response = urllib.request.urlopen(request)
+    print(response)
+
 cli.add_command(init_db)
+cli.add_command(push_transcriptions)
 
 if __name__ == '__main__':
     cli()
