c91bfe4785a7c6aa3c6dc52f18ab490427125362
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-14 15:21:24 +1000

    Cleanup

--- application/alveo/datastore.py
+++ application/alveo/datastore.py
@@ -3,57 +3,30 @@ import json
 from pyalveo import *
 from flask import g, abort, jsonify
 
-from application import app, db
+from application import db
 from application.users.model import User
 from application.misc.events import handle_api_event
 
 from application.datastore.model import Datastore
 
-from struct import pack, unpack
-
 @handle_api_event('alveo', 'datastore:get')
-def alveo_retrieve(storage_key=None, revision='latest', user_id=None, download_type=None):
-    response = None
-    query = None
+def alveo_retrieve(storage_id, user_id):
+    query = Datastore.query.filter(Datastore.id == storage_id).first()
 
-    if storage_key == None and user_id == None:
-        abort(403, 'Must provide either (or both of) a storage key or user to query')
+    if query is None:
+        abort(404, "No match for the provided id")
 
-    if user_id == 'self':
-        user_id = g.user.remote_user_id
-    elif user_id is not None:
-        user_id = 'alveo:' + user_id
+    user = User.query.filter(User.id == query.user_id).first()
 
-    if storage_key is None:
-        query = Datastore.query;
-    else:
-        query = Datastore.query.filter(Datastore.key == 'alveo:'+storage_key)
+    if not user.remote_user_id.startswith("alveo"):
+        abort(403, "You don't have permission to read the storage of an external user")
 
-    if user_id is not None:
-        user = User.query.filter(User.remote_user_id == user_id).first()
-
-        if user is None:
-            abort(404, 'No user matching that ID was found')
-
-        query = query.filter(Datastore.user_id == user.id)
-
-    if revision is not None:
-        query = query.filter(Datastore.revision == revision)
-
-    query = query.all()
-    total_queries = len(query)
-
-    if total_queries > 1:
-        exports = []
-        for datastore in query:
-            exports.append(export_json(datastore, user_id))
-        response = { 'exports': exports }
-    elif total_queries == 1:
-        response = export_json(datastore=query[0], user_id=user_id, append_data=True)
-    else:
-        abort(404, 'No matches based on provided parameters')
-
-    return jsonify(response)
+    return {
+            'id': query.id,
+            'revision': query.revision,
+            'data': json.loads(query.get_data())
+            'creator': query.user_id
+        }
 
 @handle_api_event('alveo', 'datastore:post')
 def alveo_store(storage_key, storage_value):
@@ -62,7 +35,7 @@ def alveo_store(storage_key, storage_value):
     if storage_key is None or len(storage_key) < 2:
         abort(400, 'Key is invalid or too short')
 
-    storage_key = 'alveo:'+storage_key
+    storage_key = 'alveo:' + storage_key
 
     model = Datastore.query.filter(Datastore.key == storage_key).filter(Datastore.revision == revision).filter(Datastore.user_id == g.user.id).first()
     
@@ -75,18 +48,7 @@ def alveo_store(storage_key, storage_value):
 
     db.session.commit()
 
-    return jsonify(export_json(model))
-
-def export_json(datastore, user_id=None, append_data=False):
-    export = {
-            'id': datastore.key.split(":")[1],
-            'revision': datastore.revision,
-       }
-
-    if append_data:
-        export['data'] = json.loads(datastore.get_data())
-
-    if user_id is not None and user_id is not g.user.remote_user_id:
-        export['creator'] = datastore.user.remote_user_id
-
-    return export 
+    return {
+            'id': model.id,
+            'revision': model.revision
+        }
