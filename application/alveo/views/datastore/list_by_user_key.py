b8ef885f00a129ae7865bdc75405504acc55d697
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-14 14:40:27 +1000

    Fix signature

--- application/alveo/views/datastore/list_by_user_key.py
+++ application/alveo/views/datastore/list_by_user_key.py
@@ -1,6 +1,6 @@
 from application.misc.events import handle_api_event
 from .helper_lists import datastore_list
 
-@handle_api_event('alveo', 'datastore:list_key')
-def alveo_datastore_list_key(user_id, key, revision):
+@handle_api_event('alveo', 'datastore:list_by_user_key')
+def alveo_datastore_list_by_user_key(user_id, key, revision):
     return datastore_list(user_id=user_id, key=key, revision=revision)
