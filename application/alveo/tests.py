ca81ea642a44765ab3d20c5b6ad94c62767267c3
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-16 12:14:58 +1000

    Test zip contents

--- application/alveo/tests.py
+++ application/alveo/tests.py
@@ -226,10 +226,21 @@ class AlveoTests(unittest.TestCase):
                 and response_1['list'][0]['id'] != response_2['list'][0]['id']
             ), 'Expected two separate storage objects that have the same key but differing revisions and values.')
 
-    def testGetListByRevision(self):
-        DATA_AMOUNT = 12
-        for i in range(DATA_AMOUNT):
+    def testExport(self):
+        DATA_AMOUNT = 10
+        for i in range(int(DATA_AMOUNT / 2)):
             self.postRandomData()
+
+        response_query, dataset_1 = self.postRandomData(True)
+        response_1, status = response_query;
+        self.assertEqual(200, status, 'Expected OK status when attempting to post valid data while logged in.')
+
+        for i in range(int(DATA_AMOUNT / 2)):
+            self.postRandomData()
+
+        response_query, dataset_2 = self.postRandomData(True)
+        response_2, status = response_query;
+        self.assertEqual(200, status, 'Expected OK status when attempting to post valid data while logged in.')
 
         data, is_json, status = self.get_file_response('/datastore/export/', DEFAULT_HEADERS)
         self.assertEqual(200, status, 'Expected OK status when attempting to export valid data while logged in.')
@@ -237,7 +248,16 @@ class AlveoTests(unittest.TestCase):
 
         zip_archive = zipfile.ZipFile(io.BytesIO(data), "a", zipfile.ZIP_DEFLATED, False)
         archive_names = zip_archive.namelist()
-        self.assertTrue(len(archive_names) == DATA_AMOUNT, 'Expected same amount of exported as amount posted.')
+        archive_data = zip_archive.namelist()
+        self.assertEqual(len(archive_names),  DATA_AMOUNT+2, 'Expected same amount of exported as amount posted.')
+
+        with zip_archive.open('alveo:%s.json' % dataset_1['key']) as myfile:
+            zip_json = json.loads(myfile.read())
+            self.assertEqual(len(zip_json['transcription']), len(json.dumps(dataset_1['value'])))
+
+        with zip_archive.open('alveo:%s.json' % dataset_2['key']) as myfile:
+            zip_json = json.loads(myfile.read())
+            self.assertEqual(len(zip_json['transcription']), len(json.dumps(dataset_2['value'])))
 
     """
     def testSegmentationNoAuth(self):
