ca3106881f9881bae81fc1b6c9de60044d907b6c
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-16 15:15:27 +1000

    Fix incorrect signature

--- application/datastore/views/list_by_user.py
+++ application/datastore/views/list_by_user.py
@@ -3,12 +3,11 @@ from flask import abort, jsonify
 from application.misc.event_router import EventRouter
 
 class APIListByUser(EventRouter):
-    def get(self, user_id):
+    def get(self, user_id, revision=None):
         if user_id is None:
             abort(400, "User not specified")
 
         response = self.event("datastore:list_by_user").handle(
-                key=key,
                 user_id=user_id,
                 revision=revision
             )
