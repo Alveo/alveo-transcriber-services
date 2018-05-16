c14b22ba406c691607d97785d1b4370ebd0fa8ad
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-16 10:47:06 +1000

    Improve zip export, add local dump command

--- utility.py
+++ utility.py
@@ -7,6 +7,7 @@ import click
 
 from application import app, db
 from application.datastore.model import Datastore
+from application.users.model import User 
 
 @click.group()
 def cli():
@@ -16,31 +17,50 @@ def cli():
 def init_db():
     db.drop_all()
     db.create_all()
-    click.echo("Database has been rebuilt.")
+    click.echo('Database has been rebuilt.')
+
+def export_storage():
+    datastore = Datastore.query.all()
+    archive = io.BytesIO()
+
+    with zipfile.ZipFile(archive, mode='w') as zf:
+        for data in datastore:
+            user = User.query.filter(data.user.id == User.id).first()
+            entry = {
+                    'id': data.key,
+                    'creator': {
+                            'local_id': user.id,
+                            'remote_id': user.remote_id,
+                            'domain': user.domain
+                        },
+                    'revision': data.revision,
+                    'transcription': json.loads(data.get_data())
+                    }
+            zf.writestr('%s.json' % data.key, json.dumps(entry, indent=4))
+    archive.seek(0)
+
+    return archive
+
+@click.command()
+@click.argument('path')
+def dump_storage(path):
+    archive = export_storage()
+
+    with open(path, 'wb') as f_out:
+        f_out.write(archive.getvalue())
 
 @click.command()
 @click.argument('url')
-def push_transcriptions(url):
-    data = Datastore.query.all()
-
-    archive = io.BytesIO()
-    with zipfile.ZipFile(archive, mode='w') as zf:
-        for transcription in data:
-            data = {
-                    "id": transcription.key,
-                    "creator": transcription.user.remote_user_id,
-                    "revision": transcription.revision,
-                    "transcription": transcription.value.decode()
-                    }
-            zf.writestr('%s.json' % transcription.key, json.dumps(data))
-    archive.seek(0)
+def push_storage(url):
+    archive = export_storage()
 
     request = urllib.request.Request(url, data=archive)
     response = urllib.request.urlopen(request)
     print(response)
 
 cli.add_command(init_db)
-cli.add_command(push_transcriptions)
+cli.add_command(dump_storage)
+cli.add_command(push_storage)
 
 if __name__ == '__main__':
     cli()
