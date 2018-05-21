8eecf656ad6e797086c3579d6aafa363c37b94d1
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-21 15:22:00 +1000

    Update revision test

--- application/alveo/tests/list_routes.py
+++ application/alveo/tests/list_routes.py
@@ -49,34 +49,40 @@ class AlveoListRoutesTests(AlveoTests):
 
     def testGetListByRevision(self):
         DATA_AMOUNT = 6
-        REVISION_NAME = "test_revision"
-        REVISION_NAME_2 = REVISION_NAME+"_backup"
         KEY = str(uuid.uuid4())
 
         for i in range(int(DATA_AMOUNT / 2)):
             self.postRandomData()
 
-        dataset_1 = self.generateSamplePostData(key=KEY, revision=REVISION_NAME)
-        response_1, status = self.post_json_request('/datastore/', json.dumps(dataset_1), self.DEFAULT_HEADERS)
+        dataset = self.generateSamplePostData(key=KEY)
+        response, status = self.post_json_request('/datastore/', json.dumps(dataset), self.DEFAULT_HEADERS)
         self.assertEqual(200, status, 'Expected OK status when attempting to post valid data while logged in.')
+        rev_1 = response['revision']
 
         for i in range(int(DATA_AMOUNT / 2)):
             self.postRandomData()
 
-        dataset_2 = self.generateSamplePostData(key=KEY, revision=REVISION_NAME_2)
-        response, status = self.post_json_request('/datastore/', json.dumps(dataset_2), self.DEFAULT_HEADERS)
+        dataset = self.generateSamplePostData(key=KEY)
+        response, status = self.post_json_request('/datastore/', json.dumps(dataset), self.DEFAULT_HEADERS)
         self.assertEqual(200, status, 'Expected OK status when attempting to post valid data while logged in.')
+        rev_2 = response['revision']
 
-        response_1, status = self.get_json_response('/datastore/list/%s/%s'%(KEY, REVISION_NAME), self.DEFAULT_HEADERS)
+        dataset = self.generateSamplePostData(key=KEY)
+        response, status = self.post_json_request('/datastore/', json.dumps(dataset), self.DEFAULT_HEADERS)
+        self.assertEqual(200, status, 'Expected OK status when attempting to post valid data while logged in.')
+        rev_3 = response['revision']
+
+        response_1, status = self.get_json_response('/datastore/list/%s/%s'%(KEY, rev_1), self.DEFAULT_HEADERS)
         self.assertEqual(200, status, 'Expected OK status when attempting to get valid data while logged in.')
 
-        response_2, status = self.get_json_response('/datastore/list/%s/%s'%(KEY, REVISION_NAME_2), self.DEFAULT_HEADERS)
+        response_2, status = self.get_json_response('/datastore/list/%s/%s'%(KEY, rev_2), self.DEFAULT_HEADERS)
         self.assertEqual(200, status, 'Expected OK status when attempting to get valid data while logged in.')
 
         self.assertTrue( (
                 response_1['query_key'] == response_2['query_key']
-                and response_1['query_revision'] == REVISION_NAME
-                and response_2['query_revision'] == REVISION_NAME_2
+                and response_1['query_revision'] == rev_1
+                and response_2['query_revision'] == rev_2
+                and rev_3 not in [response_1['query_revision'], response_2['query_revision']]
                 and response_1['storage_objects'][0]['id'] != response_2['storage_objects'][0]['id']
             ), 'Expected two separate storage objects that have the same key but differing revisions and values.')
 
@@ -120,7 +126,7 @@ class AlveoListRoutesTests(AlveoTests):
         response, status = self.get_json_response('/datastore/?store_id=%s'%query_key, self.DEFAULT_HEADERS)
         self.assertEqual(200, status, 'Expected OK status when attempting to get valid data while logged in.')
 
-        self.assertEqual(len(str(response['data'])), len(transcription_2.get_value()), 'Expected the queried data to match the other user\'s data.')
+        self.assertEqual(len(str(response['transcription'])), len(transcription_2.get_value()), 'Expected the queried data to match the other user\'s data.')
 
     def testOtherDomainListFail(self):
         self.generateSampleAlveoData()
