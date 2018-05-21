0f3d2dc2096c0334df9b53d7c4791514d7746350
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-21 13:55:39 +1000

    Enforce unique constraint on combination of (domain, remote_user_id)

--- application/users/model.py
+++ application/users/model.py
@@ -4,11 +4,16 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     domain = db.Column(db.String(192), nullable=False)
     remote_id = db.Column(db.String(128), nullable=False)
+    __table_args__ = (
+                        db.UniqueConstraint(
+                            'remote_id',
+                            'domain',
+                            name='_remote_user_uc'),
+                     )
 
     def __init__(self, remote_id, domain):
         self.remote_id = remote_id
         self.domain = domain
-        # TODO prevent duplicates
 
     def __repr__(self):
         return 'User id %s (%s@%s)' % (self.id, self.remote_id, self.domain)
