1761d157770657425e2202ce847406e5b2add538
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-07-30 11:56:52 +1000

    Export more helpful information in lists, support user as opt argument

--- application/alveo/views/datastore/helper_lists.py
+++ application/alveo/views/datastore/helper_lists.py
@@ -5,8 +5,12 @@ def datastore_list(user_id=None, object_key=None):
     query_data = datastore_query(user_id=user_id, key=object_key)
     list_data = []
     for data in query_data:
+        total_versions = data.versions.count()
         list_data.append({
             'id': data.id,
+            'version': total_versions - 1,
+            'total_versions': total_versions,
+            'timestamp': data.timestamp.isoformat()
         })
 
     data = {
--- application/datastore/view_wrappers/list_by_key.py
+++ application/datastore/view_wrappers/list_by_key.py
@@ -1,16 +1,24 @@
-from flask import abort, jsonify, g
+from flask import abort, jsonify, request, g
 
 from application.misc.query_wrapper import QueryWrapper
 
 
 class ListByKeyWrapper(QueryWrapper):
     def get(self, object_key=None):
-        if object_key is None:
-            abort(400, "Key not specified")
+        user_id = request.args.get('user_id')
+
+        #if object_key is None:
+        #    abort(400, "Key not specified")
+
+        if user_id is None:
+            if g.user is not None:
+                user_id = g.user.id
+            else:
+                abort(401, "You must log in to do that")
 
         response = self._processor_get(
             object_key=object_key,
-            user_id=g.user.id
+            user_id=user_id
         )
 
         return jsonify(response)
