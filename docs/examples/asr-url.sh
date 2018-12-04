d62ec649ffee5f62a008e250a4f201dd01a2ed36
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-12-04 13:26:24 +1100

    Create asr example

---
+++ docs/examples/asr-url.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Set environment variables for:
+#  ATS_API_DOMAIN, e.g app.alveo.edu.au
+#  ATS_API_KEY, e.g a valid Alveo API key
+#  ATS_URL, e.g https://segmenter.apps.alveo.edu.au/alveo
+
+# Attempts to authenticate and send an Alveo URL via param $1 for ASR
+# Example usage:
+#   sh asr-url.sh https://app.alveo.edu.au/catalog/austalk/1_1274_2_7_001/document/1_1274_2_7_001-ch6-speaker16.wav
+url=$1
+
+curl \
+  --header "X-Api-Domain: $ATS_API_DOMAIN" \
+  --header "X-Api-Key: $ATS_API_KEY" \
+  $ATS_URL/asr?remote_url=$url
