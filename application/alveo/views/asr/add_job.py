e94d7690ed2696525d9896c5933d1d6e2823e614
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-12-07 15:55:59 +1100

    Rewrite get method, implementing gcloud queue

--- application/alveo/views/asr/add_job.py
+++ application/alveo/views/asr/add_job.py
@@ -8,13 +8,16 @@ from application.users.model import User
 from application.jobs.model import Job
 from application.jobs.types import JobTypes
 
-from application.alveo.module import shorten_path
+from application.alveo.module import shorten_path, DOMAIN
 from application.alveo.views.access import verify_access
+from application.alveo.views.asr.helper_exporter import export_asrdata
 from application.auth.required import auth_required
 from application.alveo.services import transcribe_document
 from application.datastore.binary import create_binary_object, get_binary_object
 from application.asr.view_wrappers.add_job import AddJobWrapper
 from application.asr.engines.gcloud.speech import transcribe
+
+ENGINE = "asr-engine-gcloud"
 
 def test():
     active_job = get_current_job()
@@ -29,6 +32,7 @@ def test():
     print(job.datastore.get_value())
     print(job.status)
     print("Done")
+    #    create_binary_object(short_path, result)
 
 class AlveoASRAddJobRoute(AddJobWrapper):
     decorators = [
@@ -41,33 +45,37 @@ class AlveoASRAddJobRoute(AddJobWrapper):
         api_key = g.user.remote_api_key
         verify_access(remote_path, api_key)
 
-        # Enqueued the function
-        worker = redis_queue.enqueue(test)
-        ds = Datastore("justatest-%s" % uuid4(), "{}", "", g.user, "Noalias")
+        short_path = shorten_path(remote_path)
+
+        # Check cache first
+        cached_asr = Datastore.query.filter(
+            Datastore.key == '%s:%s:%s' % (DOMAIN, ENGINE, short_path)).first()
+        if cached_asr is not None:
+            return {
+                "status": "cached",
+                "result": export_asrdata(cached_asr)
+            }
+
+        worker = redis_queue.enqueue(transcribe_document, (remote_path, api_key))
+        ds = Datastore(
+            key="%s:%s:%s" % (DOMAIN, ENGINE, short_path),
+            value="{}",
+            storage_spec="asr-engine-gcloud/json/1.0",
+            user=g.user,
+            alias="init"
+        )
         job = Job(
             external_id=worker.id,
             user=g.user,
             datastore=ds,
-            description="This is just a test job"
+            description="ASR via engine 'gcloud' for item: %s" % short_path
         )
         db.session.add(ds)
         db.session.add(job)
         db.session.commit()
 
-        # Create the job
         return {"status": "queued", "job_id": job.id}
 
-        #short_path = shorten_path(remote_path)
-
-        #result = get_binary_object(short_path)
-        #if result is None:
-        #result = transcribe_document(remote_path, api_key)
-        #if result is None:
-        #    abort(400, 'Could not access requested document')
-        #else:
-        #    create_binary_object(short_path, result)
-
-        #return result
 
     def _processor_post(self, user_id, audiofile):
         result = transcribe(audiofile.read())
